d75e2da870f8686481e5f54eb4abd7bae8c33840
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-22 21:57:49 +0100

    imported Jumbotron, Container, Row, Col from react-bootstrap

--- react-portfolio-site/src/components/Hero.js
+++ react-portfolio-site/src/components/Hero.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function Hero() {
+function Hero(props) {
 
   return(
-    <p>Hero Works</p>
+    <Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            { props.title && <h1>(props.title)</h1> }
+            { props.subTitle && <h3>(props.subTitle)</h3> }
+            { props.text && <h3>(props.text)</h3> }
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
   );
 
 }
