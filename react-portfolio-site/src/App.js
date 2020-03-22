9b2f1d49d0498763bbff8b69295bf80756d671a5
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-22 12:41:14 +0100

    created home, about, contact elements

--- react-portfolio-site/src/App.js
+++ react-portfolio-site/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Leo Costa",
+      headerLinks: [
+        { title: "Home", path: '/' },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+      ],
+      home: {
+        title: "Leo"s Personal Website",
+        subTitle: "My Projects",
+        descriptionText: "Checkout"
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Contact Me",
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello From React</div>
+    );
+  }
 }
 
 export default App;
