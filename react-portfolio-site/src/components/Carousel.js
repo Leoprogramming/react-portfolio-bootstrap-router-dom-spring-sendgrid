78c1ec7776d83341fb72b8add60f457ecf02a9e1
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-24 17:06:26 +0100

    fixed indentation

--- react-portfolio-site/src/components/Carousel.js
+++ react-portfolio-site/src/components/Carousel.js
@@ -13,66 +13,68 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        items: [
-            {
-                id: 0,
-                title: 'GitHub',
-                subTitle: 'Some Of My Past Production',
-                imgSrc: Github,
-                link: 'https://devgrub.com',
-                selected: false
-            },
-            {
-                id: 1,
-                title: 'CodePen',
-                subTitle: 'Past Previous',
-                imgSrc: Codepen,
-                link: 'https://www.youtube.com/channel/UCxSITxL2JbF229OGCqieVZw',
-                selected: false
-            },
-            {
-                id: 2,
-                title: 'LinkedIn',
-                subTitle: 'A social network for developers',
-                imgSrc: Profile,
-                link: 'https://github.com/garrettlove8/evverest',
-                selected: false
-            },
-        ]
-    }
+      items: [
+      {
+        id: 0,
+        title: 'GitHub',
+        subTitle: 'Some Of My Past Production',
+        imgSrc: Github,
+        link: 'https://devgrub.com',
+        selected: false
+      },
+
+      {
+        id: 1,
+        title: 'CodePen',
+        subTitle: 'Past Previous',
+        imgSrc: Codepen,
+        link: 'https://www.youtube.com/channelUCxSITxL2JbF229OGCqieVZw',
+        selected: false
+      },
+
+      {
+        id: 2,
+        title: 'LinkedIn',
+        subTitle: 'A social network for developers',
+        imgSrc: Profile,
+        link: 'https://github.com/garrettlove8/evverest',
+        selected: false
+      },
+    ]
+  }
 }
 
 
 handleCardClick = (id, card) => {
 
-    let items = [...this.state.items];
+  let items = [...this.state.items];
 
-    items[id].selected = items[id].selected ? false : true;
+  items[id].selected = items[id].selected ? false : true;
 
-    items.forEach(item => {
-        if(item.id !== id) {
-            item.selected = false;
-        }
-    });
+  items.forEach(item => {
+      if(item.id !== id) {
+          item.selected = false;
+      }
+  });
 
-    this.setState({
-        items
-    });
+  this.setState({
+      items
+  });
 }
 
 makeItems = (items) => {
-    return items.map(item => {
-        return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
-    })
+  return items.map(item => {
+      return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+  })
 }
 
 render() {
-    return(
-        <Container fluid={true}>
-            <Row className="justify-content-around">
-                {this.makeItems(this.state.items)}
-            </Row>
-        </Container>
+  return(
+      <Container fluid={true}>
+          <Row className="justify-content-around">
+              {this.makeItems(this.state.items)}
+          </Row>
+      </Container>
     );
   }
 
