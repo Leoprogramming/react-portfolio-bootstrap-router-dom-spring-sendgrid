58010ab2a6e1c1b8d474134668bf3eb860bc7922
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-25 17:58:57 +0100

    imported packages and stablished API headers and get method on app.js

--- portfolio-api/app.js
+++ portfolio-api/app.js
@@ -1,5 +1,22 @@
 const express = require("express");
-const bodyParser = require("bodyParser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const sendGrid = require("@sendGrid/mail");
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/api", (req, res, next) => {
+  res.send("API Status: Running")
+})
